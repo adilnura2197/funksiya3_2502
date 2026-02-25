d5e8977cb8a8a64d98d594bb9d1cb9adaa1a6abc
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-02-25 16:36:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+#1
+def yigindi(a, b):
+    return a + b
+
+res = yigindi(13, 4)
+print(f"Ikki raqam yig'indisi: {res}")
+
+
+#2
+def kvadrat(a):
+    return a * a
+
+res = kvadrat(10)
+print(f"Raqam kvadrati: {res}")
+
+
+#3
+def ism(a):
+    return len(a)
+
+res = ism("Dilnura")
+print(f"Ism uzunligi: {res}")
+
+
+#4
+def katta(a, b):
+    return max(a, b)
+
+res = katta(13, 8)
+print(f"Eng katta raqam: {res}")
+
+
+#5
+def matn(a):
+    return f"Sizning matningiz {a}"
+
+res = matn("Hello")
+print(res)
+
+
+#6
+def farqi(a, b):
+    return a - b
+
+res = farqi(12, 8)
+print(f"Ikki raqam farqi: {res}")
+
+
+#7
+def kopaytma(a, b):
+    return a * b
+
+res = kopaytma(3, 5)
+print(f"Ikki raqam ko'paytmasi: {res}")
+
+
+#8
+def bolinma(a, b):
+    return a // b
+
+res = bolinma(81, 9)
+print(f"Ikki raqam bo'linmasi: {res}")
+
+
+#9
+def salom(a):
+    return f"Salom! {a}"
+
+res = salom("Dilnura")
+print(res)
+
+
+#10
+def ism(a):
+    return f"Sizning ismingiz: {a}"
+
+
+res = ism("Dilnura")
+print(res)
